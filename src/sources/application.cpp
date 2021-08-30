7ca9d36865a13b130b94a450cd339d2417845164
Jakub 'Aquaver' Wyciślik <aquaverpolska@gmail.com>
2021-08-30 08:01:50 +0200

    Added constructor in application class source file

---
+++ src/sources/application.cpp
@@ -0,0 +1,12 @@
+#include "src/headers/application.h"
+#include "ui_application.h"
+
+Application::Application(QWidget* parent) : QMainWindow(parent), ui(new Ui::Application) {
+  ui -> setupUi(this);
+
+  initValues(); // Must be first to prepare all values for chart initialization.
+  initChartField();
+  initSlots();
+  initToolbar();
+  initTabOrder();
+}
