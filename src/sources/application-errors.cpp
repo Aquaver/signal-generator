0bc8bd48ddd0116725ca447ca428a5a38364b4fa
Jakub 'Aquaver' Wyciślik <aquaverpolska@gmail.com>
2021-09-04 12:04:23 +0200

    Added adding chart error in application (errors) class source file

---
+++ src/sources/application-errors.cpp
@@ -0,0 +1,15 @@
+#include "src/headers/application.h"
+
+#include <QMessageBox>
+
+void Application::errorAddingChart() {
+  QMessageBox error;
+  QString line1 = "Invalid signal parameter value";
+  QString line2 = "One of the parameters contains invalid or empty characters";
+
+  error.setIcon(QMessageBox::Icon::Critical);
+  error.setText(line1 + '\n' + line2);
+  error.setWindowTitle("Signal Generator - Error");
+
+  error.exec();
+}
