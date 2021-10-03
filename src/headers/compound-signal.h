afb6b7815116b901405a48ab94d0c0f64a66869b
Jakub 'Aquaver' Wyciślik <aquaverpolska@gmail.com>
2021-10-03 14:54:57 +0200

    Added public members in compound signal class header file

--- src/headers/compound-signal.h
+++ src/headers/compound-signal.h
@@ -4,4 +4,7 @@
 #include "src/headers/signal.h"
 
 class CompoundSignal : public Signal {
+  public:
+    void setCompoundSignals(Signal* first, Signal* second);
+    void setCompoundType(CompoundType newType);
 };
