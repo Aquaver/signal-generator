119ad662394cf2fcf4c54c9804fb2f0f7954109b
Jakub 'Aquaver' Wyciślik <aquaverpolska@gmail.com>
2021-10-08 08:02:10 +0200

    Added signal function in compound signal class source file

--- src/sources/compound-signal.cpp
+++ src/sources/compound-signal.cpp
@@ -8,3 +8,19 @@ void CompoundSignal::setCompoundSignals(Signal *first, Signal *second) {
 void CompoundSignal::setCompoundType(CompoundType newType) {
   type = newType;
 }
+
+qreal CompoundSignal::signalFunction(qreal argument) {
+  if (firstSignal != nullptr && secondSignal != nullptr) {
+    if (type == CompoundType::SUM) {
+      return firstSignal -> signalFunction(argument) + secondSignal -> signalFunction(argument);
+    } else if (type == CompoundType::DIFFERENCE) {
+      return firstSignal -> signalFunction(argument) - secondSignal -> signalFunction(argument);
+    } else {
+      perror("[Debug Error] Invalid compound type");
+      return -1;
+    }
+  } else {
+    perror("[Debug Error] One of signals has not been initialized (or both of them)");
+    return -1;
+  }
+}
