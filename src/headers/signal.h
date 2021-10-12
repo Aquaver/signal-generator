d0c12fcfeb00b72ef51f7f00032af491cf0db879
Jakub 'Aquaver' Wyciślik <aquaverpolska@gmail.com>
2021-10-12 08:04:45 +0200

    Added public members in signal class header file

--- src/headers/signal.h
+++ src/headers/signal.h
@@ -4,4 +4,12 @@
 #include <QVector>
 
 class Signal {
+  public:
+    bool isSelected = false;
+
+    QVector<qreal> getArguments();
+    QVector<qreal> getValues();
+
+    void setArguments(QVector<qreal> newArguments);
+    qint32 signalLength(); // Returns length of arguments/values set (they should be equal - if not then this function returns -1 which means an error).
 };
