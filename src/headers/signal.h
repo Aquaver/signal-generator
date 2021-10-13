45d3a3fda0d98b793865f5747a0191398a85fb23
Jakub 'Aquaver' Wyciślik <aquaverpolska@gmail.com>
2021-10-13 08:19:30 +0200

    Added protected members in signal class header file

--- src/headers/signal.h
+++ src/headers/signal.h
@@ -12,4 +12,13 @@ class Signal {
 
     void setArguments(QVector<qreal> newArguments);
     qint32 signalLength(); // Returns length of arguments/values set (they should be equal - if not then this function returns -1 which means an error).
+  
+  protected:
+    static qreal shiftX, shiftY;
+
+    QVector<qreal> arguments;
+    QVector<qreal> values;
+
+    virtual qreal signalFunction(qreal argument) = 0; // Master signal function (mathematical model) which converts an argument into proper value.
+    virtual void calculateValues(); // Computes values vector from arguments vector.
 };
