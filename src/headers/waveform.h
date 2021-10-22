9b4db7bb5618470cfcd2ac12909405d396f33d7a
Jakub 'Aquaver' Wyciślik <aquaverpolska@gmail.com>
2021-10-22 07:56:15 +0200

    Added sine class public members in waveform class header file

--- src/headers/waveform.h
+++ src/headers/waveform.h
@@ -3,4 +3,7 @@
 #include "src/headers/signal.h"
 
 class Sine : public Signal {
+  public:
+    Sine() : amplitude{0}, constant{0}, frequency{0}, phase{0} {}
+    Sine(qreal amplitude, qreal constant, qreal frequency, qreal phase);
 };
