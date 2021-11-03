15a54461fb22e325ed5292327aa88866faba65c7
Jakub 'Aquaver' Wyciślik <aquaverpolska@gmail.com>
2021-11-03 08:02:24 +0100

    Added square class constructor in waveform class source file

--- src/sources/waveform.cpp
+++ src/sources/waveform.cpp
@@ -4,3 +4,10 @@
 Sine::Sine(qreal amplitude, qreal constant, qreal frequency, qreal phase) : amplitude{amplitude}, constant{constant}, frequency{frequency}, phase{phase} {
   // Sinusoidal waveform does not have any additional parameter value limits.
 }
+
+Square::Square(qreal amplitude, qreal constant, qreal dutyCycle, qreal period, qreal phase) : amplitude{amplitude}, constant{constant}, dutyCycle{dutyCycle}, period{period}, phase{phase} {
+  if (dutyCycle < 0 || dutyCycle > 100) {
+    perror("[Debug Error] Created Square waveform with invalid duty cycle");
+    dutyCycle = 50;
+  }
+}
